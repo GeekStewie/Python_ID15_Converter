36c6a77319ed61c5afcd6a6d2ce463bc59215d48
Stewart Anderson <stewart.anderson@salesforce.com>
2023-08-21 10:53:46 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def convert_to_18_char(sf_id):
+    if len(sf_id) == 18:
+        return sf_id
+    if len(sf_id) != 15:
+        raise ValueError("Salesforce ID must be either 15 or 18 characters in length.")
+    
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    
+    def calculate_position(segment):
+        position = 0
+        for idx, char in enumerate(segment):
+            if 'A' <= char <= 'Z':
+                position += 2 ** idx
+        return chars[position]
+
+    suffix = ''.join(calculate_position(sf_id[i:i+5]) for i in range(0, 15, 5))
+    
+    return sf_id + suffix
